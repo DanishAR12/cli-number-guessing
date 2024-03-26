7040683c0849348f49a8252e8fbb6c48a9e33672
DanishAR12 <160243905+DanishAR12@users.noreply.github.com>
2024-03-26 14:58:21 +0500

    created number guessing game

---
+++ index.ts
@@ -0,0 +1,18 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+
+const answers = await inquirer.prompt([
+  {
+    message: "please guess a number between 1-6:",
+    type: "number",
+    name: "userGuessNumber",
+  },
+]);
+
+if (answers.userGuessNumber === randomNumber){
+    console.log("congtratulation! you guess right number");
+}else{
+    console.log("you gueses wrong number try again");
+}
